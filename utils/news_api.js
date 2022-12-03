4700cab49bd84acbbed3283742ebd909397ccae6
Abdullah Omar <fishilike.fish@gmail.com>
2022-12-03 08:48:24 +0600

    feat: #8 api to search news from everywhere

--- utils/news_api.js
+++ utils/news_api.js
@@ -1,9 +1,9 @@
 const newsApiToken = '262fb595c4aa4a41985245b3f3eb6306';
 
-const getLatestNews = async () => {
+const searchNews = async (searchString) => {
 	try {
 		let res = await fetch(
-			`https://newsapi.org/v2/top-headlines?country=us&sortBy=popularity&apiKey=${newsApiToken}&pageSize=20`
+			`https://newsapi.org/v2/everything?sortBy=popularity&q=${searchString}&apiKey=${newsApiToken}&pageSize=20`
 		);
 
 		res = await res.json();
@@ -20,4 +20,24 @@ const getLatestNews = async () => {
 	}
 };
 
-export { getLatestNews };
+const getNewsByCategory = async (category) => {
+	try {
+		let res = await fetch(
+			`https://newsapi.org/v2/top-headlines?country=us&sortBy=popularity&category=${category}&apiKey=${newsApiToken}&pageSize=20`
+		);
+
+		res = await res.json();
+
+		if (res.status === 'error') {
+			return {
+				error: res.message,
+			};
+		}
+
+		return res;
+	} catch (error) {
+		console.error('🚀 -> file: news_api.js:7 -> getNewsByCategory -> error', error);
+	}
+};
+
+export { searchNews, getNewsByCategory };
